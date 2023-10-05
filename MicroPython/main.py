d8e58bc4ca90cd154d1d46833151ebbb78daabb8
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-05 17:29:36 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,4 +7,30 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        pin13.write_digital(1)
+        display.scroll('Red')
+        pin13.write_digital(0)
+        pin14.write_digital(1)
+        display.scroll('Blue')
+        pin14.write_digital(0)
+        pin15.write_digital(1)
+        display.scroll('Green')
+        pin15.write_digital(0)
+        pin13.write_digital(1)
+        pin14.write_digital(1)
+        display.scroll('Magenta')
+        pin14.write_digital(0)
+        pin13.write_digital(1)
+        pin15.write_digital(1)
+        display.scroll('Yellow')
+        pin13.write_digital(0)
+        pin14.write_digital(1)
+        display.scroll('Cyan')
+        pin13.write_digital(1)
+        display.scroll('White')
+        pin13.write_digital(0)
+        pin14.write_digital(0)
+        pin15.write_digital(0)
+        display.show(Image.HAPPY)
