22eba8668555d14bfd28268008a59649706bc182
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-09 13:11:37 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Ihor Chernyshev
  * Created on: Oct 2023
- * This program shows all the colors created by the RGB LED.
+ * This program shows all the colors created by the RGB LED
 */
 
 basic.clearScreen()
@@ -19,11 +19,11 @@ input.onButtonPressed(Button.A, function () {
   basic.showString('Blue')
   pins.digitalWritePin(DigitalPin.P14, 0)
   pins.digitalWritePin(DigitalPin.P15, 1)
-  basic.showString('Blue')
-  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString('Green')
+  pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P13, 1)
   pins.digitalWritePin(DigitalPin.P14, 1)
-  basic.showString('Magneta')
+  basic.showString('Magenta')
   pins.digitalWritePin(DigitalPin.P14, 0)
   pins.digitalWritePin(DigitalPin.P15, 1)
   basic.showString('Yellow')
@@ -32,7 +32,7 @@ input.onButtonPressed(Button.A, function () {
   basic.showString('Cyan')
   pins.digitalWritePin(DigitalPin.P13, 1)
   basic.showString('White')
-  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P13, 0)
   pins.digitalWritePin(DigitalPin.P14, 0)
   pins.digitalWritePin(DigitalPin.P15, 0)
   basic.showIcon(IconNames.Happy)
